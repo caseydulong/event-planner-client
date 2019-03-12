ba85f2fa0fe34d0c749412f82a22ea5c8efa42db
caseydulong <caseydulong@gmail.com>
2019-03-11 22:40:16 -0400

    Adds Heroku production server URL to config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://limitless-thicket-65278.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
